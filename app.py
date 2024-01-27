d15cf916943c0674138949be4fbeda780e07b143
abhishek <abhishek123789@gmail.com>
2024-01-27 23:38:59 +0530

    add new field in user modal for created and adding data in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///firstdatabase.db'
@@ -7,15 +8,18 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow())
     def __repr__(self):
         return '<User %r>' % self.username
 
 
 @app.route('/')
 def hello_world():
+    user = User(username="Abhishek", email="abhishek123789@gmail.com")
+    db.session.add(user)
+    db.session.commit()
     return 'Hello, World!'
 
 if __name__ == "__main__":
