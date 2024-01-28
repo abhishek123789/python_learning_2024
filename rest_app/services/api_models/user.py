0ec02b489e50b235bc025f2db63241ebb38efaee
abhishek <abhishek123789@gmail.com>
2024-01-28 18:14:17 +0530

    API modal that will be shown in swagger ui page

--- rest_app/services/api_models/user.py
+++ rest_app/services/api_models/user.py
@@ -1,7 +1,8 @@
 from flask_restx import fields
+from rest_app.database_extensions import api
 
-user_model = {
+user_model = api.model('User', {
     "id": fields.Integer(description="User ID"),
     "username": fields.String(description="Username of the user"),
     "email": fields.String(description="Email address of the user")
-}
+})
